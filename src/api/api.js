0b8be17ee8bd2ab9a61aea455171cd4e07fad96f
OmDhavale <om.17933@sakec.ac.in>
2026-03-06 16:20:43 +0530

    deploy - baseURL

--- src/api/api.js
+++ src/api/api.js
@@ -10,7 +10,8 @@ const DEV_MACHINE_IP = '172.16.5.114';
 const BASE_HOST = Platform.OS === 'android' ? DEV_MACHINE_IP : 'localhost';
 
 const api = axios.create({
-    baseURL: `http://${BASE_HOST}:5000/api`,
+    // baseURL: `http://${BASE_HOST}:5000/api`,
+    baseURL: `https://art-finance-management.onrender.com/`,
     headers: {
         'Content-Type': 'application/json',
     },
